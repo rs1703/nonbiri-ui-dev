e6a334e8509a01daca094c63fc769e58a16addee
rs <96896020+rs1703@users.noreply.github.com>
2022-04-24 14:54:05 +0700

    add filter apply and reset event listeners

--- src/components/Browse/Header.ts
+++ src/components/Browse/Header.ts
@@ -23,17 +23,90 @@ const SearchForm = () => {
   submit.type = "submit";
   submit.appendChild(Search());
 
-  form.append(input, submit);
-
   form.addEventListener("submit", ev => {
     ev.preventDefault();
 
     const url = new URL(window.location.href);
     url.searchParams.set("q", input.value);
 
-    Router.navigate(url.pathname + url.search, { preventDefault: true });
+    Router.navigate(url.pathname + url.search, {
+      preventDefault: true
+    });
   });
+
+  form.append(input, submit);
   return form;
+};
+
+const Filters = (filters: Filter[]) => {
+  if (!filters?.length) {
+    return undefined;
+  }
+
+  const container = document.createElement("div");
+  container.classList.add("filters");
+
+  filters.forEach(filter => {
+    container.appendChild(Filter(filter));
+  });
+
+  const applyBtn = document.createElement("button");
+  applyBtn.classList.add("apply-btn");
+  applyBtn.type = "button";
+  applyBtn.textContent = "Apply";
+
+  const ignore = ["q", "id"];
+  applyBtn.addEventListener("click", ev => {
+    ev.preventDefault();
+
+    const url = new URL(window.location.href);
+    Array.from(url.searchParams.keys()).forEach(key => {
+      if (!ignore.includes(key)) {
+        url.searchParams.delete(key);
+      }
+    });
+
+    container.querySelectorAll("input, select").forEach(element => {
+      if (element instanceof HTMLInputElement && element.checked) {
+        if (element.type === "checkbox") {
+          url.searchParams.append(element.name, element.value);
+        } else if (element.type === "radio") {
+          url.searchParams.set(element.name, element.value);
+        }
+      } else if (element instanceof HTMLSelectElement && element.value) {
+        url.searchParams.set(element.name, element.value);
+      }
+    });
+
+    Router.navigate(url.pathname + url.search, {
+      preventDefault: true
+    });
+  });
+
+  const resetBtn = document.createElement("button");
+  resetBtn.classList.add("reset-btn");
+  resetBtn.type = "button";
+  resetBtn.textContent = "Reset";
+
+  resetBtn.addEventListener("click", ev => {
+    ev.preventDefault();
+
+    const url = new URL(window.location.href);
+    container.querySelectorAll("input, select").forEach(element => {
+      if (element instanceof HTMLInputElement) {
+        if (element.type === "checkbox") {
+          element.checked = url.searchParams.getAll(element.name).includes(element.value);
+        } else {
+          element.checked = url.searchParams.get(element.name) === element.value;
+        }
+      } else if (element instanceof HTMLSelectElement) {
+        element.value = url.searchParams.get(element.name) || element.options[0].value;
+      }
+    });
+  });
+
+  container.append(applyBtn, resetBtn);
+  return container;
 };
 
 const Header = async () => {
@@ -42,29 +115,9 @@ const Header = async () => {
 
   const header = document.createElement("header");
   header.classList.add("header");
+
   header.appendChild(SearchForm());
-
-  if (filters?.length) {
-    const filterContainer = document.createElement("div");
-    filterContainer.classList.add("filters");
-
-    filters.forEach(filter => {
-      filterContainer.appendChild(Filter(filter));
-    });
-
-    const applyBtn = document.createElement("button");
-    applyBtn.classList.add("apply-btn");
-    applyBtn.type = "button";
-    applyBtn.textContent = "Apply";
-
-    const resetBtn = document.createElement("button");
-    resetBtn.classList.add("reset-btn");
-    resetBtn.type = "button";
-    resetBtn.textContent = "Reset";
-
-    filterContainer.append(applyBtn, resetBtn);
-    header.appendChild(filterContainer);
-  }
+  header.appendChild(Filters(filters));
 
   DOM.getContainer().appendChild(header);
 };
