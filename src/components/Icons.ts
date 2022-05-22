1b836fe3a2b16d17d0e340f7266c1d45d3c11520
rs <96896020+rs1703@users.noreply.github.com>
2022-05-22 17:31:27 +0700

    udpate Icons.ts

--- src/components/Icons.ts
+++ src/components/Icons.ts
@@ -1,61 +1,40 @@
-let cachedCog: SVGSVGElement;
-
 interface Options {
   size?: number;
   strokeWidth?: number;
 }
 
-const applyOptions = (svg: SVGElement, opts?: Options) => {
-  const size = opts?.size.toString() || "24";
-  const strokeWidth = opts?.strokeWidth.toString() || "2";
-  svg.setAttribute("width", size);
-  svg.setAttribute("height", size);
-  svg.setAttribute("stroke-width", strokeWidth);
-};
-
-export const CogIcon = (opts?: Options) => {
-  if (cachedCog) {
-    return cachedCog;
-  }
-
+const createSvg = (option?: Options) => {
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("viewBox", "0 0 24 24");
   svg.setAttribute("fill", "none");
   svg.setAttribute("stroke", "currentColor");
   svg.setAttribute("stroke-linecap", "round");
   svg.setAttribute("stroke-linejoin", "round");
-  applyOptions(svg, opts);
 
+  const size = option?.size.toString() || "24";
+  svg.setAttribute("width", size);
+  svg.setAttribute("height", size);
+  svg.setAttribute("stroke-width", option?.strokeWidth.toString() || "2");
+
+  return svg;
+};
+
+export const CogIcon = (option?: Options) => {
   const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   circle.setAttribute("cx", "12");
   circle.setAttribute("cy", "12");
   circle.setAttribute("r", "3");
 
   const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path.setAttribute(
-    "d",
-    "M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"
-  );
+  path.setAttribute("d", "M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z");
 
+  const svg = createSvg(option);
   svg.append(circle, path);
-  cachedCog = svg;
+
   return svg;
 };
 
-let cachedSearch: SVGSVGElement;
-export const SearchIcon = (opts?: Options) => {
-  if (cachedSearch) {
-    return cachedSearch;
-  }
-
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("viewBox", "0 0 24 24");
-  svg.setAttribute("fill", "none");
-  svg.setAttribute("stroke", "currentColor");
-  svg.setAttribute("stroke-linecap", "round");
-  svg.setAttribute("stroke-linejoin", "round");
-  applyOptions(svg, opts);
-
+export const SearchIcon = (option?: Options) => {
   const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
   circle.setAttribute("cx", "11");
   circle.setAttribute("cy", "11");
@@ -67,30 +46,19 @@ export const SearchIcon = (opts?: Options) => {
   line.setAttribute("x2", "16.65");
   line.setAttribute("y2", "16.65");
 
+  const svg = createSvg(option);
   svg.append(circle, line);
-  cachedSearch = svg;
+
   return svg;
 };
 
-let cachedFilter: SVGSVGElement;
-export const FilterIcon = (opts?: Options) => {
-  if (cachedFilter) {
-    return cachedFilter;
-  }
-
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("viewBox", "0 0 24 24");
-  svg.setAttribute("fill", "none");
-  svg.setAttribute("stroke", "currentColor");
-  svg.setAttribute("stroke-linecap", "round");
-  svg.setAttribute("stroke-linejoin", "round");
-  applyOptions(svg, opts);
-
+export const FilterIcon = (option?: Options) => {
   const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
   polygon.setAttribute("points", "22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3");
 
+  const svg = createSvg(option);
   svg.append(polygon);
-  cachedFilter = svg;
+
   return svg;
 };
 
